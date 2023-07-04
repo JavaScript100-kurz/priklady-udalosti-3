dc46724a6bad80e8150eee00ff74856786877622
Luděk Roleček <ludek@rolecek.cz>
2023-07-04 19:05:48 +0200

    Aktualizace

--- script.js
+++ script.js
@@ -1,2 +1,46 @@
 console.log('%c Výběr více prvků na stránce ', 'background:black; color:lime;');
 
+
+const jmena = ['Anna', 'Eva', 'Jana', 'Jirka', 'Klotylda'];
+
+// console.log( jmena[2] );
+// console.log( jmena.length );
+
+
+for (let i = 0; i < jmena.length; i++) {
+	console.log( jmena[i] );
+}
+
+jmena.forEach(
+	(jmeno) => {
+		console.log(`Ahoj, ja jsem ${jmeno}`);
+	}
+);
+
+
+
+const odstavec = document.querySelector('p');
+odstavec.style.color = 'red';
+odstavec.textContent = 'Toto je odstavec';
+
+console.log(odstavec);
+
+
+const tlacitko = document.querySelector('button');
+
+tlacitko.addEventListener(
+	'click',
+	() => {
+		console.log('klik na tlacitko');
+	}
+);
+
+
+const texty = document.querySelectorAll('p');
+console.log( texty );
+
+for (let i = 0; i < texty.length; i++) {
+	texty[i].style.color = 'dodgerblue';
+}
+
+texty.forEach( (text) => { text.style.color = 'orange'; } );
